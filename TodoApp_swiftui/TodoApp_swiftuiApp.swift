1f1f51828a398616687897c70931032700132a7f
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-09-30 19:39:00 +0900

    Initial Commit

---
+++ TodoApp_swiftui/TodoApp_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_swiftuiApp.swift
+//  TodoApp_swiftui
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
